14c5ff0dc158cd8e2798742a0c0148e8ae192b01
Joaofelipe14 <jaofelpe14@gmai.com>
2023-01-20 14:14:23 -0300

    Listando produtor por categoria

--- Backend/src/routes.ts
+++ Backend/src/routes.ts
@@ -9,6 +9,7 @@ import { CreateCategoryController } from './controllers/category/CreateCategoryC
 import { ListCategoryController } from './controllers/category/ListCaretoryController'
 
 import { CreateProductController } from './controllers/product/CreateProductController';
+import { ListByCategoryController } from './controllers/product/ListByCategoryController'
 
 import {isAuthenticated} from './milddlewares/isAuthenticated'
 
@@ -31,6 +32,6 @@ router.get('/category', isAuthenticated, new ListCategoryController().handle )
 
 //-- ROTAS PRODUCT
 router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle )
-
+router.get ('/category/product', isAuthenticated, new ListByCategoryController().handle)
 
 export { router };
