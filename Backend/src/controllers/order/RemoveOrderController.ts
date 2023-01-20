da070ec3f25f1071f43da63627763d9ac4531f8c
Joaofelipe14 <jaofelpe14@gmai.com>
2023-01-20 15:48:28 -0300

    Criando ordem dE pedidos

---
+++ Backend/src/controllers/order/RemoveOrderController.ts
@@ -0,0 +1,20 @@
+import { Request,Response } from "express";
+import {RemoverOderService} from '../../services/order/RemoverOrderService'
+
+class RemoverOrderController {
+    async handle(req: Request, res: Response ){
+        const order_id = req.query.order_id as string;
+
+        const removeOrder = await new RemoverOderService();
+
+        const order = removeOrder.execute({
+            order_id
+        });
+
+        return res.json(order);
+
+    }
+}
+
+
+export { RemoverOrderController }
