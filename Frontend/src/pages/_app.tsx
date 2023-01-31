eb3b8263d35f88d882040976aa86b8158454d00f
Joaofelipe14 <jaofelpe14@gmai.com>
2023-01-31 15:47:25 -0300

    Implementando parte do login e formulario de novo usuario

--- Frontend/src/pages/_app.tsx
+++ Frontend/src/pages/_app.tsx
@@ -1,9 +1,14 @@
+import '../../styles/globals.scss'
+import { AppProps } from 'next/app';
 
-import type { AppProps } from 'next/app'
+import { AuthProvider } from '../contexts/AuthContext'
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+   <AuthProvider>
+      <Component {...pageProps} />
+   </AuthProvider>
+  )
 }
 
-
-//export default MyApp
+export default MyApp
--- Frontend/src/pages/index.tsx
+++ Frontend/src/pages/index.tsx
@@ -1,7 +1,66 @@
+import { useContext, FormEvent } from 'react'
 
+import Head from 'next/head'
+import Image from 'next/image';
+import styles from '../../styles/home.module.scss';
+
+import logoImg from '../../public/logo.svg';
+
+import { Input } from '../components/ui/Input'
+import { Button } from '../components/ui/Button'
+
+import { AuthContext } from '../contexts/AuthContext'
+
+import Link from 'next/link';
 
 export default function Home() {
+  const { signIn } = useContext(AuthContext)
+
+  async function handleLogin(event: FormEvent){
+    event.preventDefault();
+
+    let data = {
+      email: "algum@teste.com",
+      password: "123123"
+    }
+
+    await signIn(data)
+  }
+
   return (
-  <div><h1>Sujeito pizza</h1></div>
+    <>
+    <Head>
+      <title>SujeitoPizza - Faça seu login</title> 
+    </Head>
+    <div className={styles.containerCenter}>
+      <Image src={logoImg} alt="Logo Sujeito Pizzaria" />
+
+      <div className={styles.login}>
+        <form onSubmit={handleLogin}>
+          <Input
+            placeholder="Digite seu email"
+            type="text"
+          />
+
+          <Input
+            placeholder="Sua senha"
+            type="password"
+          />
+          
+          <Button
+            type="submit"
+            loading={false}
+          >
+            Acessar
+          </Button>
+        </form>
+
+        <Link className={styles.text} href="/signup">
+           Nao possui uma conta? Cadastre-se
+        </Link>
+
+      </div>
+    </div>
+    </>
   )
 }
